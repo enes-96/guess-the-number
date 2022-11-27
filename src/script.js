82a774f82dc5b6600eb4b3a9108eb77e8ff26d51
Seyit Enes <seyit7enes@gmail.com>
2022-11-27 22:41:40 +0100

    working on the game

--- src/script.js
+++ src/script.js
@@ -1,7 +1,26 @@
 "use strict";
 const btnCheck = document.querySelector("#btn-check");
 const userInput = document.querySelector("#user-input");
+const secretNumber = document.querySelector("#secret-number");
+const hint = document.querySelector("#hint");
 
-const secretNum = function () {
-  return Math.floor(Math.random() * 21 + 1);
-};
+let randomNum = Math.floor(Math.random() * 21 + 1);
+let userScore = 0;
+let userLife = 3;
+
+btnCheck.addEventListener("click", () => {
+  if (userInput.value == randomNum) {
+    secretNumber.innerHTML = randomNum;
+    hint.innerHTML = "true!!!";
+  } else if (userInput.value > randomNum) {
+    hint.innerHTML = "TOO HIGH!!";
+    userLife--;
+  } else if (userInput.value < randomNum) {
+    hint.innerHTML = "TOO LOW!!";
+    userLife--;
+  }
+  if (userLife === 0) {
+    console.log("you lost ");
+    return;
+  }
+});
